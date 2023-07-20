c101ffe0c6b6af3bafd07108b46cef4cb0b04795
Musosiyare <musosiyarethacien10@gmail.com>
2023-07-20 06:45:06 +0200

    footer commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
                     on a user's keyword input, sorts the search results by IMDb ratings, 
                     and returns the top-rated movie along with its details. You can enhance
                      this system by incorporating user preferences, collaborative filtering,
-                      or content-based filtering techniques for more advanced recommendations.
+                      or content-based filtering techniques for more advanced recommendations!.
                     </p>
                     <p className='my-4'>
                         <Link to='/register'>
