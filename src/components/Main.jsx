4970645f301e11368372de265131ffed0366edf4
Musosiyare <musosiyarethacien10@gmail.com>
2023-07-23 01:58:43 +0200

    saturday first commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,17 +26,12 @@ const truncateString = (str, num) => {
             <div className='w-full h-full'>
                 <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
                 <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`} alt={movie?.title} />
-                <div className='absolute w-full top-[20%] p-4 md:p-8'>
+                <div className='absolute w-full top-[20%] p-4 md:p-8 mb-5'>
                     <h1 className='font-bold text-3xl md:5xl'>{movie?.title}</h1>
-                    <div className='py-4'>
-                        <button
-                            className='border bg-gray-300 hover:bg-red-600 hover:text-white hover:border-red-600 text-black border-gray-300 py-2 px-5 rounded'>Play</button>
-                        <button
-                            className='border ml-4 text-white hover:bg-red-600 hover:border-red-600  border-gray-300 py-2 px-5 rounded'>Watch later</button>
-                    </div>
+            
                     <p className='text-orange-500 text-1xl'>This Movie Reased At : {movie?.release_date}</p>
                     <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>{
-                        truncateString(movie?.overview, 200)
+                        truncateString(movie?.overview, 500)
                     }</p>
                 </div>
             </div>
